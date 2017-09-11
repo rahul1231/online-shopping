20833fae012af88a20e27ccfe76d944b49e0c68c
Rahul S <Rahulsidhpura912@gmail.com>
2017-09-11 19:03:19 +0530

    Done as on 11-09-2017

--- shoppingbackend/src/main/java/net/kzn/shoppingbackend/dao/CategoryDAO.java
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/dao/CategoryDAO.java
@@ -6,10 +6,9 @@ import net.kzn.shoppingbackend.dto.Category;
 
 public interface CategoryDAO {
 
-	boolean add(Category category);
-	
-	
-	List<Category> list();
 	Category get(int id);
-	
+	List<Category> list();
+	boolean add(Category category);
+	boolean update(Category category);
+	boolean delete(Category category);
 }
